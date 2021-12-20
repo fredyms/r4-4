58420aca1da2c7fe441c03f4139200e43108cc7b
fredyms <91507208+fredyms@users.noreply.github.com>
2021-12-20 01:38:29 -0500

    rn3

--- src/main/java/com/retos/CacharreroApplication.java
+++ src/main/java/com/retos/CacharreroApplication.java
@@ -1,5 +1,9 @@
 package com.retos;
 
+import com.retos.repository.crud.ClotheRepository;
+import com.retos.repository.crud.OrderRepository;
+import com.retos.repository.crud.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -11,6 +15,21 @@ public class CacharreroApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(CacharreroApplication.class, args);
+    }
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Autowired
+    private ClotheRepository clotheRepository;
+
+    @Autowired
+    private OrderRepository orderRepository;
+    @Override
+    public void run(String... args) throws Exception {
+        userRepository.deleteAll();
+        clotheRepository.deleteAll();
+        orderRepository.deleteAll();
     }
 
 }
