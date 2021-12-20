844ca5cf9286da474aa9faf964ef005977c8bb33
fredyms <91507208+fredyms@users.noreply.github.com>
2021-12-20 01:42:13 -0500

    rn3

--- src/main/java/com/retos/CacharreroApplication.java
+++ src/main/java/com/retos/CacharreroApplication.java
@@ -4,6 +4,7 @@ import com.retos.repository.crud.ClotheRepository;
 import com.retos.repository.crud.OrderRepository;
 import com.retos.repository.crud.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -11,7 +12,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  * @ autor Paola Martinez
  **/
 @SpringBootApplication
-public class CacharreroApplication {
+public class CacharreroApplication implements CommandLineRunner {
 
     public static void main(String[] args) {
         SpringApplication.run(CacharreroApplication.class, args);
